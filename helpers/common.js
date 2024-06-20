b27493361845fba21f6b03b508c4ffc2ec363a55
Shaheen7a <aousshaheen7@gmail.com>
2024-06-20 23:45:30 +0300

    lib

---
+++ helpers/common.js
@@ -0,0 +1,41 @@
+import { Dimensions } from "react-native";
+
+const { width: deviceWidth, height: deviceHeight } = Dimensions.get("window");
+
+export const wp = (percentage) => {
+  const width = deviceWidth;
+  return (percentage * deviceWidth) / 100;
+};
+
+export const hp = (percentage) => {
+  const height = deviceHeight;
+  return (percentage * deviceHeight) / 100;
+};
+
+// export const getColumnsCount = () => {
+//   if (deviceWidth >= 1024) {
+//     return 4;
+//   }
+
+//   if (deviceWidth >= 768) {
+//     return 3;
+//   }
+
+//   return 2;
+// };
+
+// export const getImageSize = (height: number, width: number): number => {
+//   if (width > height) {
+//     return 250;
+//   }
+
+//   if (width < height) {
+//     return 300;
+//   }
+
+//   return 200;
+// };
+
+// export const capitalize = (str: string) => {
+//   return str.replace(/\b\w/g, (l) => l.toUpperCase());
+// };
